ec49aa8485babd6be99b77ec57de0241f0a542f6
Francis Emmanuel Calingo <83982370+Francis-Calingo@users.noreply.github.com>
2025-06-02 19:22:35 -0400

    Create deploy.R

---
+++ deploy.R
@@ -0,0 +1,11 @@
+
+
+#############################
+## PUBLISH ON shinyapps.io ##
+#############################
+
+rsconnect::setAccountInfo(name='weerou-francis-calingo',
+                          token='DFC62FC2EABFB1340029C74C081ACDE4',
+                          secret='//Rd+mdB/VBzvJrmZ1uOdoYBAN3yz6pZ8ZvlOWl+')
+
+rsconnect::deployApp('C:/Users/francali/Documents/shiny/ilocano-dashboard/')
